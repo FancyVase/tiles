422444c43f70883587ec944d5ddfb95e668a9b7e
Hiram Moncivais <mm.hiram@gmail.com>
2017-02-01 15:03:05 -0500

    Add reset functionality

---
+++ Assets/GoogleVR/Scripts/ResetScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void resetTiles() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
