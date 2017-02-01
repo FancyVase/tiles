57e2b4e2bc44088ddc051c6e0eca2a93f6994fd9
Hiram Moncivais <mm.hiram@gmail.com>
2017-02-01 01:31:32 -0500

    Initial commit

---
+++ Assets/GoogleVR/Scripts/PlanesToSphere.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanesToSphere : MonoBehaviour {
+
+	public float radius;
+
+	void Start() {
+		Vector3[] positionArr = {
+			new Vector3(0,0.5f,0),
+			new Vector3(0,-0.5f,0),
+			new Vector3(0,0,0.5f),
+			new Vector3(0,0,-0.5f),
+			new Vector3(0.5f,0,0),
+			new Vector3(-0.5f,0,0)
+		};
+
+		Vector3[] rotationArr = {
+			Vector3.zero,
+			new Vector3(180,0,0),
+			new Vector3(90,0,0),
+			new Vector3(-90,0,0),
+			new Vector3(0,0,-90),
+			new Vector3(0,0,90)
+		};
+
+		for(int p = 0; p<6; p++)
+		{
+			//make 6 planes for the sphere in this loop
+			GameObject newPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+			newPlane.transform.SetParent(transform);
+			newPlane.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+			newPlane.transform.position = positionArr[p];
+			newPlane.transform.eulerAngles = rotationArr[p];
+			Vector3[] vertices = newPlane.GetComponent<MeshFilter>().mesh.vertices;
+			for (var i = 0; i < vertices.Length; i++)
+			{
+				vertices[i] = newPlane.transform.InverseTransformPoint(newPlane.transform.TransformPoint(vertices[i]).normalized * this.radius);
+			}
+			newPlane.GetComponent<MeshFilter>().mesh.vertices = vertices;
+			newPlane.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+			newPlane.GetComponent<MeshFilter>().mesh.RecalculateBounds();
+		}
+	}
+}
---
+++ Assets/GoogleVR/Scripts/TileScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+	public int col;
+	bool isOn;
+	public AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		isOn = false;
+		StartCoroutine (PlayToneLoop ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator PlayToneLoop() {
+		yield return new WaitForSeconds(col/5f);
+		while (true) {
+//			PlaySound ();
+			MeshRenderer my_renderer = GetComponent<MeshRenderer>();
+			if (my_renderer != null) {
+				my_renderer.material.color = Color.cyan;
+			}
+			yield return new WaitForSeconds (1/5f);
+			if (my_renderer != null) {
+				my_renderer.material.color = Color.white;
+			}
+			yield return new WaitForSeconds(3);
+		}
+	}
+//	public void playSound(int tone) {
+//		playSound (tone);
+//	}
+
+	public void onClick() {
+
+		MeshRenderer my_renderer = GetComponent<MeshRenderer> ();
+		if (my_renderer != null) {
+			if (isOn) {
+				my_renderer.material.color = new Color(0.196f,0.196f,0.196f);
+			} else {
+				my_renderer.material.color = Color.white;
+			}
+			isOn = !isOn;
+		}
+	}
+}
